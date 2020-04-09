773be357463c67baad32ae35354bf75ce3bb57b3
Markus Kiili <markus.kiili2@gmail.com>
2020-04-09 08:02:59 +0200

    Clojure lein new app

---
+++ test/reittiopas_backend/core_test.clj
@@ -0,0 +1,7 @@
+(ns reittiopas-backend.core-test
+  (:require [clojure.test :refer :all]
+            [reittiopas-backend.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
